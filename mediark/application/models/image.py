e2f2cc68f952a2166ce9c742649a3abd9881946e
eecheverry <eecheverry@nubark.com>
2018-11-07 08:44:37 -0500

    Update image attributes

--- mediark/application/models/image.py
+++ mediark/application/models/image.py
@@ -1,3 +1,6 @@
 class Image:
-    def __init__(self):
-        pass
+    def __init__(self, **attributes):
+        self.namespace = attributes.get('namespace', '')
+        self.reference = attributes.get('reference', '')
+        self.extension = attributes.get('extension', 'jpg')
+        self.url = attributes.get('url', '')
--- tests/application/models/test_image.py
+++ tests/application/models/test_image.py
@@ -3,4 +3,22 @@ from mediark.application.models import Image
 
 def test_image_instantiation():
     image = Image()
-    assert image is not None
+
+    assert image.namespace == ''
+    assert image.reference == ''
+    assert image.extension == 'jpg'
+    assert image.url == ''
+
+
+def test_image_attributes():
+    image = Image(namespace='https://example.com',
+                  reference='00648c29-eca2-4112-8a1a-4deedb443188',
+                  extension='png',
+                  url=('https://mediark.knowark.com/media/images/'
+                       '00648c29-eca2-4112-8a1a-4deedb443188.png'))
+
+    assert image.namespace == 'https://example.com'
+    assert image.reference == '00648c29-eca2-4112-8a1a-4deedb443188'
+    assert image.extension == 'png'
+    assert image.url == ('https://mediark.knowark.com/media/images/'
+                         '00648c29-eca2-4112-8a1a-4deedb443188.png')
