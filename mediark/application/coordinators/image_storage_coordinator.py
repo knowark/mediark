e213fd6f2f67c4aefe8e503090165f82a0736f93
eecheverry <eecheverry@nubark.com>
2018-11-08 19:04:47 -0500

    Add file store service to image storage coordinator

--- mediark/application/coordinators/image_storage_coordinator.py
+++ mediark/application/coordinators/image_storage_coordinator.py
@@ -1,18 +1,29 @@
 from ..models import Image
 from ..repositories import ImageRepository
-from ..services import IdService
+from ..services import IdService, FileStoreService
 from .types import ImageDict
 
 
 class ImageStorageCoordinator:
     def __init__(self, image_repository: ImageRepository,
-                 id_service: IdService) -> None:
+                 id_service: IdService,
+                 file_store_service: FileStoreService) -> None:
         self.image_repository = image_repository
         self.id_service = id_service
+        self.file_store_service = file_store_service
 
-    def store(self, image_dict: ImageDict):
+    def store(self, image_dict: ImageDict) -> None:
+        if 'data' not in image_dict:
+            return
+
         if 'id' not in image_dict:
             image_dict['id'] = self.id_service.generate_id()
 
+        locator = image_dict.get('id')
+        content = image_dict.pop('data')
+
+        url = self.file_store_service.store(locator, content)
+        image_dict['url'] = url
         image = Image(**image_dict)
+
         self.image_repository.add(image)
--- tests/application/coordinators/test_image_storage_coordinator.py
+++ tests/application/coordinators/test_image_storage_coordinator.py
@@ -1,7 +1,8 @@
 from pytest import fixture
 from mediark.application.repositories import (
     MemoryImageRepository, ExpressionParser)
-from mediark.application.services import StandardIdService
+from mediark.application.services import (
+    StandardIdService, MemoryFileStoreService)
 from mediark.application.coordinators import ImageStorageCoordinator
 
 
@@ -10,16 +11,17 @@ def image_storage_coordinator():
     parser = ExpressionParser()
     image_repository = MemoryImageRepository(parser)
     id_service = StandardIdService()
+    file_store_service = MemoryFileStoreService()
 
     return ImageStorageCoordinator(
-        image_repository, id_service)
+        image_repository, id_service, file_store_service)
 
 
 def test_storage_coordinator_instantiation(image_storage_coordinator):
     assert image_storage_coordinator is not None
 
 
-def test_storage_coordinator_store(image_storage_coordinator):
+def test_storage_coordinator_store_no_data(image_storage_coordinator):
     image_dict = {
         'namespace': 'https://example.com',
         'reference': '00648c29-eca2-4112-8a1a-4deedb443188',
@@ -27,4 +29,42 @@ def test_storage_coordinator_store(image_storage_coordinator):
 
     image_storage_coordinator.store(image_dict)
 
+    assert len(image_storage_coordinator.image_repository.items) == 0
+
+
+def test_storage_coordinator_store_data(image_storage_coordinator):
+    image_dict = {
+        'namespace': 'https://example.com',
+        'reference': '00648c29-eca2-4112-8a1a-4deedb443188',
+        'data': 'BASE64_DATA',
+        'extension': 'jpg'}
+
+    image_storage_coordinator.store(image_dict)
+
     assert len(image_storage_coordinator.image_repository.items) == 1
+
+
+def test_storage_coordinator_store_file(image_storage_coordinator):
+    image_dict = {
+        'namespace': 'https://example.com',
+        'reference': '00648c29-eca2-4112-8a1a-4deedb443188',
+        'data': ('"iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFUlEQVR42'
+                 'mNk+H+1noEIwDiqkL4KAUP4F0koL9m+AAAAAElFTkSuQmCC",'),
+        'extension': 'png'}
+
+    called = False
+
+    class MockFileStoreService:
+        def store(self, locator: str, content: str):
+            nonlocal called
+            called = True
+            file_store_service = MemoryFileStoreService()
+            return file_store_service.store(locator, content)
+
+    image_storage_coordinator.file_store_service = MockFileStoreService()
+    image_storage_coordinator.store(image_dict)
+
+    assert called is True
+    image = next(
+        iter(image_storage_coordinator.image_repository.items.values()))
+    assert image.url == image.id
