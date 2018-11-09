e18aba82c9eaef74c2d37fff5dc2dc1f90c1be03
eecheverry <eecheverry@nubark.com>
2018-11-09 16:00:47 -0500

    Remove download paths

--- mediark/infrastructure/resolver/directory_factory.py
+++ mediark/infrastructure/resolver/directory_factory.py
@@ -13,12 +13,10 @@ class DirectoryFactory(ShelveFactory):
             DirectoryImageFileStoreService):
         directory = self.config['images']['media']
         extension = self.config['images']['extension']
-        download = self.config['images']['download']
-        return DirectoryImageFileStoreService(directory, extension, download)
+        return DirectoryImageFileStoreService(directory, extension)
 
     def directory_audio_file_store_service(self) -> (
             DirectoryAudioFileStoreService):
         directory = self.config['audios']['media']
         extension = self.config['audios']['extension']
-        download = self.config['audios']['download']
-        return DirectoryAudioFileStoreService(directory, extension, download)
+        return DirectoryAudioFileStoreService(directory, extension)
