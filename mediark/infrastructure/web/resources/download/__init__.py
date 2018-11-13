a11a7a6e96a141ae42e18fd8bfa6804ea3434fb8
eecheverry <eecheverry@nubark.com>
2018-11-13 18:13:35 -0500

    Fix download endpoint by removing extra return values

--- mediark/infrastructure/web/resources/download/__init__.py
+++ mediark/infrastructure/web/resources/download/__init__.py
@@ -13,5 +13,4 @@ class DownloadResource(Resource):
     @swag_from('get.yml')
     def get(self, type: str, uri: str) -> Any:
         directory = Path(self.media_directory).joinpath(type)
-        headers = {'Content-type': 'application/octet-stream'}
-        return send_from_directory(directory, uri), 200, headers
+        return send_from_directory(directory, uri)
