d29ea7e4a7d66b11fbed908d9d02af256970e7f6
jbuitron <jbuitron@servagro.com.co>
2019-06-06 08:48:04 -0500

    Modify field schema Download

--- mediark/infrastructure/web/schemas/download.py
+++ mediark/infrastructure/web/schemas/download.py
@@ -8,3 +8,9 @@ class DownloadSchema(Schema):
      url = fields.Str(
           requiered=False, example="https://mediark.knowark/media/audios/00648c29-eca2-4112-8a1a-4deedb443188.webm"
      )
+     id = fields.Str(
+          required=False, example="637250d6-dc57-4d96-9f8a-2697ca5c55c3")
+     namespace = fields.Str(
+          required=False, example="https://example.com")
+     extension = fields.Str(
+          required=False, example="webm")
