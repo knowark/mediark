c819f05d9e00e5c97f4cdad2dd873a93dcc591f7
jjalvarez <jjalvarez@servagro.com.co>
2019-09-16 12:18:41 -0500

    Directory factoring supporting tenancy

--- mediark/infrastructure/core/factories/directory_factory.py
+++ mediark/infrastructure/core/factories/directory_factory.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from ....application.utilities import (
+    TenantProvider, StandardTenantProvider)
 from ....infrastructure.data import (
     DirectoryImageFileStoreService, DirectoryAudioFileStoreService)
 from ..configuration import Config
@@ -9,14 +11,14 @@ class DirectoryFactory(ShelveFactory):
     def __init__(self, config: Config) -> None:
         super().__init__(config)
 
-    def directory_image_file_store_service(self) -> (
-            DirectoryImageFileStoreService):
-        directory = self.config['media'] + self.config['images']['media']
-        extension = self.config['images']['extension']
-        return DirectoryImageFileStoreService(directory, extension)
+    def directory_image_file_store_service(
+        self, tenant_provider: TenantProvider
+    ) -> DirectoryImageFileStoreService:
+        return DirectoryImageFileStoreService(
+            tenant_provider, self.config["data"], "images")
 
-    def directory_audio_file_store_service(self) -> (
-            DirectoryAudioFileStoreService):
-        directory = self.config['media'] + self.config['audios']['media']
-        extension = self.config['audios']['extension']
-        return DirectoryAudioFileStoreService(directory, extension)
+    def directory_audio_file_store_service(
+        self, tenant_provider: TenantProvider
+    ) -> DirectoryAudioFileStoreService:
+        return DirectoryAudioFileStoreService(
+            tenant_provider, self.config["data"], "audios")
