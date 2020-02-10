c8df4cdea38162724d52a9ae6a618c2bd6653186
jjalvarezl <jjalvarezl@unicauca.edu.co>
2020-02-10 14:23:23 -0500

    Updating configurations.

--- mediark/infrastructure/core/factories/json_factory.py
+++ mediark/infrastructure/core/factories/json_factory.py
@@ -5,11 +5,17 @@ from ....infrastructure.data import (
 from mediark.application.utilities import TenantProvider, AuthProvider
 from ..configuration import Config
 from .memory_factory import MemoryFactory
+from ...core import JsonTenantSupplier, TenantSupplier
 
 
 class JsonFactory(MemoryFactory):
     def __init__(self, config: Config) -> None:
         super().__init__(config)
+
+    def json_tenant_supplier(self) -> TenantSupplier:
+        catalog_path = self.config['tenancy']['json']
+        directory_data = self.config['data']['dir_path']
+        return JsonTenantSupplier(catalog_path, directory_data)
 
     def json_image_repository(
         self, query_parser: QueryParser, tenant_provider: TenantProvider,
