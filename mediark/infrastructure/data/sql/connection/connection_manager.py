c17ae733d858cbb5f6ca22d64f3829affa9deb61
eecheverry <eecheverry@nubark.com>
2020-03-05 12:07:16 -0500

    Fix connection manager

--- mediark/infrastructure/data/sql/connection/connection_manager.py
+++ mediark/infrastructure/data/sql/connection/connection_manager.py
@@ -47,5 +47,5 @@ class DefaultConnectionManager(ConnectionManager):
     async def _setup(self):
         self.pools = self.pools or {}
         for settings in self.settings:
-            pool_name = settings.pop('name')
+            pool_name = settings['name']
             self.pools[pool_name] = await create_pool(**settings)
