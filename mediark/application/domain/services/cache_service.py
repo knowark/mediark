3e70f06d916dfcc93e89b95f28c35999e81f9140
Esteban <tebanep@gmail.com>
2021-04-14 22:19:19 +0000

    Implement in memory lru cache with expiration

--- mediark/application/domain/services/cache_service.py
+++ mediark/application/domain/services/cache_service.py
@@ -1,3 +1,4 @@
+import time
 import collections
 from abc import ABC, abstractmethod
 
@@ -12,14 +13,31 @@ class CacheService(ABC):
         "Set method to be implemented."
 
 
-class StandardCacheService(IdService):
+class StandardCacheService(CacheService):
     """In-memory cache service using an OrderedDict."""
 
-    def __init__(self) -> None:
-        self.cache = OrderedDict()
+    def __init__(self, size=1024, lifetime=300) -> None:
+        self.cache = collections.OrderedDict()
+        self.size = size
+        self.lifetime = lifetime
 
     def get(self, key: str, default=None) -> str:
-        "Get method to be implemented."
+        if key not in self.cache:
+            return (default if default is not None
+                    else self.cache[key])
+
+        self.cache.move_to_end(key)
+        value, expiration = self.cache[key]
+        if time.time() < expiration:
+            return value
+
+        self.cache.popitem()
+        return (default if default is not None
+                else self.cache[key])
 
     def set(self, key: str, value: str) -> None:
-        "Set method to be implemented."
+        expiration = time.time() + self.lifetime
+        self.cache[key] = (value, expiration)
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.size:
+            self.cache.popitem(last=False)
